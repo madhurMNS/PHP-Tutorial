008b78c6087792160a55f100c25900f51907468c
madhur bansal <madhurbansal@mnsfashion.in>
2022-05-14 21:09:39 +0530

    added getUserById api

--- getUserById.php
+++ getUserById.php
@@ -2,22 +2,29 @@
 
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
+    include('db.php');
 
     $id = $_GET['id'];
 
-
-    include('db.php');
     $sql = "select * from person where id = ". $id;
     $res = mysqli_query($con, $sql);
     $count = mysqli_num_rows($res);
-    if($count > 0){
-        while($row = mysqli_fetch_assoc($res)){
+    if ($count > 0) {
+        while($row = mysqli_fetch_assoc($res)) {
             $arr[] = $row;
         }
-        echo json_encode(array('success' => true, 'data' => $arr ,'message' => 'Data fetch Successfully'));
-    }else{
-        echo json_encode(array('success' => false ,'message' => 'Data fetch failed'));
+        echoResponse(true, $arr, "Data fetched successfully");
+    } else {
+        echoResponse(false,"","Data fetch failed");
     }
 
-    // echo json_encode(array('success'=>true,'color'=>$color, 'message'=> 'data fetch successful'));
+    function echoResponse($success,$data ,$msg) {
+        if ($success == true) {
+            $arr = array("success"=>$success, "data"=>$data,  "message"=>$msg);
+        } else {
+            $arr = array("success"=>$success, "message"=>$msg);
+        }
+        echo json_encode($arr);
+        exit;
+    }
 ?>
